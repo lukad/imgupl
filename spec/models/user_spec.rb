02d3fec54f5d207e8174fa253c3f60277338b753
Luka Dornhecker <luka.dornhecker@gmail.com>
2015-04-06 19:09:26 +0200

    tests for finding user by login for devise

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,33 @@ RSpec.describe User, type: :model do
   context 'associations' do
     it { is_expected.to have_many(:uploads) }
   end
+
+  describe '.find_first_by_auth_conditions' do
+    let(:user) { create(:user) }
+    subject { described_class.find_first_by_auth_conditions({ login: login }) }
+
+    context 'by email' do
+      context 'found' do
+        let(:login) { user.email }
+        it { is_expected.to eq(user) }
+      end
+
+      context 'not found' do
+        let(:login) { 'foo@bar.com' }
+        it { is_expected.to be(nil) }
+      end
+    end
+
+    context 'by username' do
+      context 'found' do
+        let(:login) { user.username }
+        it { is_expected.to eq(user) }
+      end
+
+      context 'not found' do
+        let(:login) { 'foouser' }
+        it { is_expected.to be(nil) }
+      end
+    end
+  end
 end
