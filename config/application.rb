48451453dbcc54cc6f56cd3a0945886d001ed3c1
Luka Dornhecker <luka.dornhecker@gmail.com>
2015-05-04 00:48:08 +0200

    bootstrap mailer layout with previews

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,9 @@ module Imgupl
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.to_prepare do
+      Devise::Mailer.layout 'mailer'
+    end
   end
 end
