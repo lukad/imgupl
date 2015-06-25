ae0cdcdbd4e3d30e246945ff05d9c4c30cdad745
Luka Dornhecker <luka.dornhecker@gmail.com>
2015-06-25 20:05:04 +0200

    :art: move spec_helper into rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,14 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
-require 'spec_helper'
+if ENV.key? 'TRAVIS'
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+    add_filter 'app/admin'
+  end
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 
@@ -14,6 +22,14 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
 
