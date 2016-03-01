b79e5009c34ed0b31fa62b3c1734cefb67714a01
Luka Dornhecker <luka.dornhecker@gmail.com>
2016-03-01 23:04:41 +0100

    use new hash syntax...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, :alert => exception.message
+    redirect_to root_path, alert: exception.message
   end
 
   def authenticate_admin_user!
